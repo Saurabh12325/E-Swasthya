233316f28b53ec7e5a0da1de8a082cbf3068530d
Saurabh Srivastav <saurabhsri.mau@gmail.com.>
2025-05-07 02:02:52 +0530

    first commit

---
+++ src/main/java/com/Swasthya/E_swasthya/ESwasthyaApplication.java
@@ -0,0 +1,13 @@
+package com.Swasthya.E_swasthya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESwasthyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESwasthyaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Swasthya/E_swasthya/ESwasthyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Swasthya.E_swasthya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESwasthyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
